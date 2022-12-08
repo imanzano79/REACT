ee7f32ed6b1dc86a7350528d718d84b5a8454f0a
imanzano79 <imanzano@cimat.mx>
2022-12-08 13:23:57 -0600

    Correcion codigo bootstrap

--- punk-beer/src/App.js
+++ punk-beer/src/App.js
@@ -1,44 +1,42 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-import {Navbar, NavbarBrand, NavItem, Nav, Navlink, NavbarText, DropdownMenu, DropdownItem, UncontrolledDropdown, DropdownToggle  } from 'reactstrap'
+import {Navbar, NavbarBrand, NavItem, Nav, NavLink, NavbarText, DropdownMenu, DropdownItem, UncontrolledDropdown, DropdownToggle, NavbarToggler  } from 'reactstrap'
 
-const nombre = "Manzano";
+// const nombre = "Manzano";
 const brand = "We love Beer";
 
 function App() {
   return (
 
     <div className='App'>
-      <Navbar>
-        <NavbarBrand href="/" > {brand} </NavbarBrand>
-        <Nav className="me-auto" >
-        <NavItem>
-          <Navlink href="/components/">Components</Navlink>
-        </NavItem>
-        <NavItem>
-          <Navlink href="https://github.com/reactstrap/reactstrap">
-            GitHub
-          </Navlink>
-        </NavItem>
-        <UncontrolledDropdown nav inNavbar>
-          <DropdownToggle nav caret>
-            Options
-          </DropdownToggle>
-          <DropdownMenu right>
-            <DropdownItem>Options 1</DropdownItem>
-            <DropdownItem>Options 2</DropdownItem>
-            <DropdownItem divider />
-            <DropdownItem>Reset</DropdownItem>
-          </DropdownMenu>
-        </UncontrolledDropdown>
-        </Nav>
-        <NavbarText>Simple Text</NavbarText>
+        <Navbar>
+        <NavbarBrand href="/">{brand} </NavbarBrand>        
+          <Nav className="me-auto" >
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                GitHub
+              </NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
       </Navbar>
-
-    </div>
-
-   
+      
+    </div>   
   );
 }
 
